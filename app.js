41899f557b6293f79600425efd35af04c448716c
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-02-28 11:57:03 -0800

    moved static files and fixed css bug

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/",function(req,res){
+    var Name = req.body.Name;
+    var Email = req.body.Email;
+    console.log(Name+"/n"+Email);
 })
 
 app.listen(3000, function(){
